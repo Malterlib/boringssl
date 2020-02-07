ab6033582365c32e58f27e07e9f41aa54ed19a20
Erik Olofsson <erik.olofsson@favro.com>
2020-02-07 15:28:21 +0100

    Add OPENSSL_NO_SSL_TRACE to fix node compile error

--- include/openssl/opensslconf.h
+++ include/openssl/opensslconf.h
@@ -67,6 +67,7 @@
 #define OPENSSL_NO_STORE
 #define OPENSSL_NO_UI_CONSOLE
 #define OPENSSL_NO_WHIRLPOOL
+#define OPENSSL_NO_SSL_TRACE
 
 // We do not implement OpenSSL's CMS API, except for a tiny subset. Projects
 // targeting the tiny subset can define BORINGSSL_NO_NO_CMS to suppress
