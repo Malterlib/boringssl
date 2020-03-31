5d965dbb083b7049faf54b59964c036e73e4b7bf
Erik Olofsson <erik@olofsson.info>
2020-03-31 08:00:09 +0200

    Fix module exit memory leaks.
    These memory leaks are important when linking to BoringSSL statically in a dynamically loaded module.

--- crypto/thread_pthread.c
+++ crypto/thread_pthread.c
@@ -95,6 +95,9 @@ void CRYPTO_once(CRYPTO_once_t *once, void (*init)(void)) {
   }
 }
 
+void CRYPTO_add_cleanup(void (*cleanup)(void *), void *context) {
+}
+
 static pthread_mutex_t g_destructors_lock = PTHREAD_MUTEX_INITIALIZER;
 static thread_local_destructor_t g_destructors[NUM_OPENSSL_THREAD_LOCALS];
 
