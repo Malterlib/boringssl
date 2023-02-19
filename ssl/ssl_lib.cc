eb520486e000d10f41a405ea4f1ca02d690e292b
Erik Olofsson <erik.olofsson@favro.com>
2023-02-19 19:49:12 +0100

    Fix node break
    Revert "Track SSL_ERROR_ZERO_RETURN explicitly."
    
    This reverts commit ebd8b8965c74ab06bb91f7a00b23822e1f1f26ca.

--- ssl/ssl_lib.cc
+++ ssl/ssl_lib.cc
@@ -1241,7 +1241,7 @@ int SSL_get_error(const SSL *ssl, int ret_code) {
   }
 
   if (ret_code == 0) {
-    if (ssl_impl->s3->rwstate == SSL_ERROR_ZERO_RETURN) {
+    if (ssl_impl->s3->read_shutdown == ssl_shutdown_close_notify) {
       return SSL_ERROR_ZERO_RETURN;
     }
     // An EOF was observed which violates the protocol, and the underlying
@@ -2831,14 +2831,7 @@ void *SSL_CTX_get_ex_data(const SSL_CTX *ctx, int idx) {
   return CRYPTO_get_ex_data(&FromOpaque(ctx)->ex_data, idx);
 }
 
-int SSL_want(const SSL *ssl) {
-  const auto *ssl_impl = FromOpaque(ssl);
-  // Historically, OpenSSL did not track `SSL_ERROR_ZERO_RETURN` as an `rwstate`
-  // value. We do, but map it back to `SSL_ERROR_NONE` to preserve the original
-  // behavior.
-  return ssl_impl->s3->rwstate == SSL_ERROR_ZERO_RETURN ? SSL_ERROR_NONE
-                                                        : ssl_impl->s3->rwstate;
-}
+int SSL_want(const SSL *ssl) { return FromOpaque(ssl)->s3->rwstate; }
 
 void SSL_CTX_set_tmp_rsa_callback(SSL_CTX *ctx,
                                   RSA *(*cb)(SSL *ssl, int is_export,
